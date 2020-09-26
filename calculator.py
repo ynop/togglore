5357fe37176217b8e3bd24681a66265e21da3380
Riccardo Salzer <salzamt@live.com>
2020-09-26 21:01:13 +0200

    classify

--- calculator.py
+++ calculator.py
@@ -2,15 +2,18 @@ import base64
 import datetime
 import itertools
 import json
+import json
 import logging
 import os
+import pickle
 import urllib
 import urllib.request
+from datetime import date, timedelta
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 from urllib.parse import urlparse
 from workalendar.europe import Austria
-from datetime import date, timedelta
+
 from togglore.toggl import TogglClient
 from togglore.utils import DateRange
 
@@ -26,7 +29,6 @@ cal = Austria()
 
 # assign the order of work days, starting a week with monday (0-7)
 MON, TUE, WED, THU, FRI, SAT, SUN = range(1, 8)
-working_days = [MON, TUE, THU, FRI]
 
 # calculate vacation days
 
@@ -47,82 +49,161 @@ vacations = [
     (datetime.date(2020, 9, 4), datetime.date(2020, 9, 4)),
 ]
 
-vacation_days = []
-for sdate, edate in vacations:
-    delta = edate - sdate
-    for i in range(delta.days + 1):
-        vd = sdate + timedelta(days=i)
-        vacation_days.append(vd)
-        print(f'vacation day: {vd}')
-print(f'vacation days entered: {len(vacation_days)}')
 
-today = datetime.date.today()
-employment_started = datetime.date(2017, 9, 15)
-employment_duration = datetime.date.today() - employment_started
-
-workdays = []
-non_working_days = []
-countable_vacation_days=[]
-
-ref = employment_started - relativedelta(days=1)
-while ref < today:
-    ref = ref + relativedelta(days=1)
-    if not cal.is_working_day(ref):
-        reason = cal.get_holiday_label(ref) or 'weekend.'
-        print(f'-- {ref} is {reason}')
-        non_working_days.append(ref)
-        continue
-    if ref.isoweekday() not in working_days:
-        print(f'-- {ref} is not in your working days.')
-        non_working_days.append(ref)
-        continue
-    if ref in vacation_days:
-        print(f'-- {ref} on vacation.')
-        countable_vacation_days.append(ref)
-        continue
-
-    print(f'++ {ref} is a working day.')
-    workdays.append(ref)
-
-should_worked_h = len(workdays) * 8
+def save_obj(obj, name):
+    with open(f'.cache_{name}.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
-time_entries = []
-for ws in tc.workspaces:
-    from_date = employment_started
-    to_date = employment_started + relativedelta(years=1)
-    while True:
-        logger.info(f'query workspace {ws} from {from_date} to {to_date}')
-        print(f'query workspace {ws} from {from_date} to {to_date}')
-        te = tc.time_entries(
-            ws, DateRange(from_date, to_date)
-        )
-        time_entries.extend(te)
+def load_obj(name):
+    with open(f'.cache_{name}.pkl', 'rb') as f:
+        return pickle.load(f)
 
-        # if the last to_date was including today, we are done here.
-        if to_date > today:
-            break
-
-        from_date = to_date + relativedelta(days=1)
-        to_date = from_date + relativedelta(years=1)
-
-user_entries = []
-for entry in time_entries:
-    if entry['uid'] != tc.user_id:
-        logger.debug(f'ignoring timestamp from {entry["user"]}')
-        print(f'ignoring timestamp from {entry["user"]}')
-        continue
-    user_entries.append(entry)
-dur = 0
-
-for te in user_entries:
-    dur += te['dur']
-
-worked_h = dur / 3600 / 1000
-
-print(f'{int(worked_h - should_worked_h)}h overtime.')
-print(f'{len(countable_vacation_days)} countable vacation days.')
 
 class LifeWorkBalance(object):
-    def __init__(self):
-        pass
+    vacations = None
+    vacation_days_total = None
+
+    cal_vacation_days_countable = None
+    cal_non_working_days = None
+    cal_working_days = None
+    time_entries = None
+
+    def __init__(
+            self, calendar, client, vacations, employment_started,
+            weekly_working_days, refresh=False):
+
+        self.client = client
+        self.cal = calendar
+        self.vacations = vacations
+        self.weekly_working_days = weekly_working_days
+        self.today = datetime.date.today()
+        self.employment_started = employment_started
+
+
+        self._deflate_vacations()
+        self._fetch_time_entries(refresh)
+        self._generate_calendar()
+
+    @property
+    def employment_duration(self):
+        return datetime.date.today() - self.employment_started
+
+    @property
+    def total_seconds_worked(self):
+        dur = 0
+        for te in self.time_entries:
+            dur += te['dur']
+        return dur
+
+    @property
+    def total_hours_worked(self):
+        return self.total_seconds_worked / 3600 / 1000
+
+    @property
+    def hours_should_worked(self):
+        return len(self.cal_working_days) * 8
+
+    @property
+    def overtime(self):
+        return int(self.total_hours_worked - self.hours_should_worked)
+
+    @property
+    def used_vacation(self):
+        return len(self.cal_vacation_days_countable)
+
+    @property
+    def vacation_days_claim(self):
+        return (int(self.employment_duration.days / 365) + 1) \
+               * 25 / 5 * len(self.weekly_working_days)
+
+    def _deflate_vacations(self):
+        vacation_days = []
+        for sdate, edate in self.vacations:
+            delta = edate - sdate
+            for i in range(delta.days + 1):
+                vd = sdate + timedelta(days=i)
+                vacation_days.append(vd)
+                print(f'vacation day: {vd}')
+        print(f'vacation days entered: {len(vacation_days)}')
+        self.vacation_days_total = vacation_days
+
+    def _fetch_time_entries(self, refresh=False):
+        cache_name = 'toggle_entries'
+        if not refresh:
+            try:
+                self.time_entries = load_obj(cache_name)
+                print('using cache instead of querying toggl.')
+                return
+            except Exception as ex:
+                print(f'cannot load cache {ex}')
+
+        time_entries = []
+        for ws in self.client.workspaces:
+            from_date = self.employment_started
+            to_date = self.employment_started + relativedelta(years=1)
+            while True:
+                print(f'query workspace {ws} from {from_date} to {to_date}')
+                te = tc.time_entries(
+                    ws, DateRange(from_date, to_date)
+                )
+                time_entries.extend(te)
+
+                # if the last to_date was including today, we are done here.
+                if to_date > self.today:
+                    break
+
+                from_date = to_date + relativedelta(days=1)
+                to_date = from_date + relativedelta(years=1)
+
+        user_entries = []
+        for entry in time_entries:
+            if entry['uid'] != self.client.user_id:
+                logger.debug(f'ignoring timestamp from {entry["user"]}')
+                print(f'ignoring timestamp from {entry["user"]}')
+                continue
+            user_entries.append(entry)
+        self.time_entries = user_entries
+        save_obj(user_entries, cache_name)
+
+    def _generate_calendar(self):
+        self.cal_working_days = []
+        self.cal_non_working_days = []
+        self.cal_vacation_days_countable = []
+        ref = self.employment_started - relativedelta(days=1)
+        while ref < self.today:
+            ref = ref + relativedelta(days=1)
+            if not cal.is_working_day(ref):
+                reason = cal.get_holiday_label(ref) or 'weekend.'
+                print(f'-- {ref} is {reason}')
+                self.cal_non_working_days.append(ref)
+                continue
+            if ref.isoweekday() not in self.weekly_working_days:
+                print(f'-- {ref} is not in your working days.')
+                self.cal_non_working_days.append(ref)
+                continue
+            if ref in self.vacation_days_total:
+                print(f'-- {ref} on vacation.')
+                self.cal_vacation_days_countable.append(ref)
+                continue
+
+            print(f'++ {ref} is a working day.')
+            self.cal_working_days.append(ref)
+
+
+balance = LifeWorkBalance(
+    calendar=cal, client=tc,
+    vacations=vacations,
+    employment_started=datetime.date(2017, 9, 15),
+    weekly_working_days=[MON, TUE, THU, FRI],
+    # refresh=True
+)
+
+print(f'{balance.overtime}h overtime.')
+print(
+    f'{balance.used_vacation} vacation days of '
+    f'{balance.vacation_days_claim} used.'
+)
+
+print(balance.calendar)
+
